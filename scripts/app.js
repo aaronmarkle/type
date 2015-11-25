c0cc876101cd94d8174db494e7fd43922ee87771
aaronmarkle <aaronkmarkle@gmail.com>
2015-11-25 13:00:33 -0800

    testToArray function converts test copy to array and sets the first letter as the currentLetter.

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,30 @@ var testCopy = document.getElementById("game-copy");
 
 $("#alphabet").click(function() {
   testCopy.textContent = alphabet;
+  testToArray();
 });
 
 $("#beginner").click(function() {
   testCopy.textContent = beginner;
+  testToArray();
 });
 
 $("#expert").click(function() {
   testCopy.textContent = expert;
+  testToArray();
 });
+
+/* Steps for game
+
+Grab current test text
+Convert to array with each letter character being its own item
+Set first letter to currentLetter
+When currentLetter key pressed, go to next letter
+
+*/
+
+var testToArray = function() {
+  var testArray = testCopy.textContent.split('');
+  var currentLetter = testArray[0];
+  console.log(currentLetter);
+}
