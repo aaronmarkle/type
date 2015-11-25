b1cedcea5e8c09ceba9083cd6fdbfc8c46a6f4aa
aaronmarkle <aaronkmarkle@gmail.com>
2015-11-25 12:37:54 -0800

    Buttons change test content on click. #5

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,18 @@
-var testCopy = document.getElementById("game-copy").textContent;
-console.log(testCopy);
+// Typing test difficulties declared
+var alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+var beginner = "cat dog hello me you yes no hi fun"
+var expert = "difficult mix floor table window pez food carpet"
+
+var testCopy = document.getElementById("game-copy");
+
+$("#alphabet").click(function() {
+  testCopy.textContent = alphabet;
+});
+
+$("#beginner").click(function() {
+  testCopy.textContent = beginner;
+});
+
+$("#expert").click(function() {
+  testCopy.textContent = expert;
+});
