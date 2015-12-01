bb6d170fb1cbbe5368cb9a5c3591124bdf8d0718
aaronmarkle <aaronkmarkle@gmail.com>
2015-12-01 15:20:07 -0800

    Added word-wrap: break-word to game screen so alphabet test does not need spaces in between the letters.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // Typing game difficulties declared
-var alphabet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'
 var beginner = 'cat dog hello me you yes no hi fun'
 var expert = 'difficult mix floor table window pez food fishy'
 
